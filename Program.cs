c124f0ab687b016f883526772dc41fe7fbaf5554
SaltedFishy03 <math740v@elev.techcollege.dk>
2022-08-23 14:33:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+bool isNumeric;
+ConsoleKeyInfo esc;
+
+do
+{
+    Console.Clear();
+
+    Console.Write("skriv et tal: ");
+    //double tal = Convert.ToDouble(Console.ReadLine());
+
+    isNumeric = double.TryParse(Console.ReadLine(), out double tal);
+
+    if (!isNumeric)
+    {
+        Console.WriteLine("Indsæt kun tal!");
+        Console.WriteLine("tryk på enter for at prøve igen");
+        Console.WriteLine("\n (tryk på esc for at afslutte programmet)");
+        continue;
+    }
+
+    double restTal = tal % 2;
+
+    if (restTal == 0)
+    {
+        Console.WriteLine("dette tal er lige");
+    }
+
+    else
+    {
+        Console.WriteLine("tal er ulige");
+    }
+
+    Console.WriteLine("tryk på esc for at afslutte programmet");
+
+} while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+
